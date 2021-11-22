ab3852e12dbd79064c833f3126f8e95d3f65ca4d
d0nc1h0t <udincev@yandex.ru>
2021-11-22 14:09:23 +0300

    Version 1.7.3+0

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ def get_safe(options, name):
         return None
 
 class MimallocConan(ConanFile):
-    version = "1.7.2+0"
+    version = "1.7.3+0"
     name = "mimalloc"
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
