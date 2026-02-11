fd62aae28c4d8f222b70b92c65796fa7598199e2
d0nc1h0t <udincev@yandex.ru>
2026-02-11 11:27:12 +0300

    Version 3.2.8-rc3+0

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conan.tools import build, microsoft, scm, env, files
 import os, shutil, functools
 
 class MimallocConan(ConanFile):
-    version = "2.2.4+0"
+    version = "3.2.8-rc3+0"
     name = "mimalloc"
     license = "MIT"
     url = "https://github.com/conan-io/conan-center-index"
